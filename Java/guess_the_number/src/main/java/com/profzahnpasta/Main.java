b09ffae509358f598fee1446c0a6cf3e2bbddf39
ProfZahnpasta <jonas@schnurstracks.de>
2024-12-18 17:55:45 +0100

    rweft

--- Java/guess_the_number/src/main/java/com/profzahnpasta/Main.java
+++ Java/guess_the_number/src/main/java/com/profzahnpasta/Main.java
@@ -1,11 +1,14 @@
 package com.profzahnpasta;
-import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        openUI();
+       /*  Scanner scanner = new Scanner(System.in);
         Integer number = ThreadLocalRandom.current().nextInt(0, 101);
         boolean guessedCorrectly = false;
         Integer attempts = 0;
@@ -16,7 +19,32 @@ public class Main {
             int usernumber = Integer.parseInt(strnumber);
             guessedCorrectly = guess(number, usernumber, attempts);
         }
-        scanner.close();
+        scanner.close(); */
+    }
+
+    public static void openUI() {
+        JFrame frame = new JFrame("Guess the number!");
+        frame.setSize(300,400);
+        frame.setLocation(100,150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultLookAndFeelDecorated(true);
+        frame.setVisible(true);
+
+        JLabel text = new JLabel("Guess a number:");
+        text.setBounds(50,25,200,30);
+        text.setHorizontalAlignment(JLabel.CENTER);
+        frame.add(text);
+
+        JTextField textField = new JTextField();
+        textField.setBounds(50,100,200,30);
+        frame.setLayout(null);
+        frame.add(textField);
+
+        JButton button = new JButton("Guess!");
+        button.setBounds(50, 150, 200,30);
+        frame.add(button);
+
+        
     }
 
     public static boolean guess(Integer number, int usernumber, Integer attempts) {
