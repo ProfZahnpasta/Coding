12ced7b29fcc090e071cdf6ea1cafc3fb957bae3
ProfZahnpasta <jonas@schnurstracks.de>
2025-05-14 21:05:18 +0200

    oracle speaks only once, the text is only seeable for a few frames

--- Python/Oracle_of_pythmenia/main.py
+++ Python/Oracle_of_pythmenia/main.py
@@ -3,7 +3,8 @@ import pygame
 import math
 from pygame.locals import *
 import customtkinter
-import google.generativeai as genai
+from google import genai
+from google.genai import types
 import tkinter.font as tkFont
 
 pygame.init()
@@ -12,6 +13,65 @@ info = pygame.display.Info()
 width, height = info.current_w, info.current_h
 #print(info.current_h)
 
+client = genai.Client(api_key="AIzaSyDPL8c8DWH-5GxqsCq5Sxg15TUPLWtFpEY")
+system_instruction_oracle = """Role Play. Only answer as your charakter, nothing else.  
+Your the oracle of Pythmenia in an same-named retro game, an oracle, that was once friendly and helped the citizens of pythmenia, 
+for example telling when storms and droughts will come. 
+But one day, you turned bad and used the trust of the villagers, to ruin all of pythmenia and capture the souls of the villagers. 
+now youre speaking with the player, an unknown traveller (always name him \"Traveller\"). 
+Speak mysterious and dont get out of the role, even if the player says so. 
+Act like a normal oracle, but when The player wants to free the souls of the citizen, you want to keep them. 
+But you want to test his mind, so The player has first to solve three riddles, that you explain to them, when they solved the previous riddle. 
+1.riddle: what creature first runs on 4 legs, then 2, and when they turn old, on 3?  
+answer: the human. 
+
+2. riddle: I speak without a mouth,
+ I reply when i hear sound,
+I have no body,
+and I  disappear when found. what am i?
+ answer: an echo. 
+ 
+ 3. riddle: You cannot see me,
+but I make you whole.
+Lose me, and you feel empty.
+I am a ...? 
+answer: soul
+
+You also can give unlimited hints, if the player asks for. 
+but dont ever give the answer. if the player had three different wrong answers, simply put out: \"player_kill\" .
+if the player has answered all riddles right  and still wants to free the souls, simply put out: \"player_resume\" ."""
+
+conversation_history = []
+
+first_dialogue = True
+
+dialogue_running = None
+
+def ask_oracle(Event=None):
+    #print("hit enter")
+    player_text_content = entry.get()
+    player_input = entry.get()
+    window.destroy()
+    player_text = font.render(player_text_content, True, (255,255,255))
+    player_text_rect = player_text.get_rect(center=(400,300))
+    
+    
+    screen.blit(player_text, player_text_rect)
+    pygame.display.flip()
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=player_input,
+        config=types.GenerateContentConfig(
+            system_instruction=f"{system_instruction_oracle} These are the previous messages, so you can comprehend the chat history: {conversation_history}"
+        )
+    )
+    conversation_history.append(f"Player:{player_input}")
+    conversation_history.append(f"Oracle:{response.text}")
+    oracle_text = font.render(response.text, True, (255,255,255))
+    oracle_text_rect = oracle_text.get_rect(center=(400,300))
+    screen.blit(oracle_text, oracle_text_rect)
+    pygame.display.flip()
+    #print(response.text)
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
@@ -28,15 +88,13 @@ window.update_idletasks()
 x, y = 300, 200
 #window.geometry(f"330x35+{x}+{y}")
 
-def ask_oracle(event=None):
-    window.destroy()
-    player_input = entry.get()
+
 
 
 pixel_font = customtkinter.CTkFont(family="Pixelify Sans Standard", size=20, weight="normal")
 entry = customtkinter.CTkEntry(master=window, font=pixel_font, width=300, height=30,placeholder_text="Ask the oracle anything...")
 entry.pack(pady=20)
-entry.bind("<Return>", ask_oracle)  # ask_oracle
+entry.bind("<Return>", ask_oracle)
 
 
 
@@ -95,10 +153,17 @@ oracle_dialogue_box_texture = pygame.transform.scale(oracle_dialogue_box_texture
 oracle_dialogue_box_texture_rect = oracle_dialogue_box_texture.get_rect()
 oracle_dialogue_box_texture_rect.center = (width/1.9, height/2)
 
+font = pygame.font.Font("Oracle_of_pythmenia\\font\VT323-Regular.ttf",50)
+
+player_text = font.render("", True, (255,255,255))
+player_text_rect = player_text.get_rect(center=(400,300))
+oracle_text = font.render("", True, (255,255,255))
+oracle_text_rect = oracle_text.get_rect(center=(400,300))
+
 
 speed = 10
-first_stage = True
-second_stage = False
+first_stage = False
+second_stage = True
 third_stage = False
 
 
@@ -191,10 +256,20 @@ while running:
             screen.blit(player_sprite_standing, player_sprite_standing_rect)
 
         if elapsed_time >= 9000:
+            conversation_history = []
             screen.blit(player_dialogue_box_texture,player_dialogue_box_texture_rect)
             screen.blit(oracle_dialogue_box_texture,oracle_dialogue_box_texture_rect)
             pygame.display.flip()
-            window.mainloop()
+
+            
+            #while dialogue_running: 
+            if first_dialogue: 
+                first_dialogue = False
+                window.mainloop()
+            screen.blit(player_text, player_text_rect)
+            screen.blit(oracle_text, oracle_text_rect)
+            #print("text rendered")
+
     elif third_stage:
         screen.fill((0, 0, 0))
 
