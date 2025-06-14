f0765624c6072430fa6a5e94b8542e9d524109e9
ProfZahnpasta <jonas@schnurstracks.de>
2025-06-14 14:10:50 +0200

    first attack done

--- Python/Oracle_of_pythmenia/main.py
+++ Python/Oracle_of_pythmenia/main.py
@@ -159,7 +159,7 @@ pygame.display.set_caption("Oracle of Pythmenia")
 
 player_sprite_standing = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/Explorer steht.png"),(525/4,882.6/4))
 player_sprite_left = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/Explorer links.png"),(525/3.6,764/3.6))
-player_sprite_right = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/Explorer rechts.PNG"),(525/3.6,765/3.6))
+player_sprite_right = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/Explorer rechts.png"),(525/3.6,765/3.6))
 player_sprite_up =  pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/Explorer oben.png"),(561,867))
 background_wall = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/Background_Wall.png"),(16000,1024))
 background_hall = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/Background_Hall.png"),(1024,1024))
@@ -170,7 +170,10 @@ background_bossfight = pygame.transform.scale(pygame.image.load("Oracle_of_pythm
 bossfight_outline = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/bossfight_outline.png"),(600,600))   
 bossfight_raw_outline = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/bossfight_raw_outline.png"),(600,600))
 dodge_item_ball = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/dodge_item_ball.png"),(100,100))
-dodge_item_flash_alert_copy = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/dodge_item_flash_alert_copy.png"),(600,600))
+dodge_item_ball1 = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/dodge_item_ball.png"),(100,100))
+dodge_item_ball2 = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/dodge_item_ball.png"),(100,100))
+dodge_item_ball3 = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/dodge_item_ball.png"),(100,100))
+dodge_item_flash_alert_copy = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/dodge_item_flash_alert.png"),(600,600))
 dodge_item_flash = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/dodge_item_flash.png"),(600,600))
 dodge_item_speer = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/dodge_item_speer.png"),(600,600))
 oracle_both_down = pygame.transform.scale(pygame.image.load("Oracle_of_pythmenia/imgs/oracle_both_down.png"),(400,400))
@@ -198,9 +201,9 @@ oracle_both_down_rect = oracle_both_down.get_rect(); oracle_both_down_rect.cente
 oracle_left_down_rect = oracle_left_down.get_rect(); oracle_left_down_rect.center = (0,0)
 oracle_right_down_rect = oracle_right_down.get_rect(); oracle_right_down_rect.center = (0,0)
 oracle_true_form_rect = oracle_true_form.get_rect(); oracle_true_form_rect.center = (0,0)
-ball1_rect = dodge_item_ball.get_rect(); ball1_rect.center = (width//2 - 150, height//2 - 200)
-ball2_rect = dodge_item_ball.get_rect(); ball2_rect.center = (width//2 - 150, height//2 - 200)
-ball3_rect = dodge_item_ball.get_rect(); ball3_rect.center = (width//2 + 150, height//2 - 200)
+ball1_rect = dodge_item_ball1.get_rect(); ball1_rect.center = (width//2 - 150, height//2 - 200)
+ball2_rect = dodge_item_ball2.get_rect(); ball2_rect.center = (width//2 - 150, height//2 - 200)
+ball3_rect = dodge_item_ball3.get_rect(); ball3_rect.center = (width//2 + 150, height//2 - 200)
 
 font = pygame.font.Font("Oracle_of_pythmenia/font/VT323-Regular.ttf",25)
 boss_text1 = font.render("You were smarter than I thought,", True, (255,255,255))
@@ -224,6 +227,7 @@ current_sprite_rect = player_sprite_standing_rect
 player_x = width // 2
 player_y = height - 160
 player_speed = 8
+dead = None
 
 #also after death
 attack_start_time = None
@@ -414,7 +418,11 @@ while running:
         if elapsed >= 5000:
             def phase1_attack_3_normal_fast_balls1():
                 global attack_start_time, attack_beginning
-                global ball1_rect, ball2_rect, ball3_rect
+                global ball1_rect, ball2_rect, ball3_rect, dodge_item_ball
+                global player_sprite_left, player_sprite_right, player_sprite_standing, player_sprite_up
+                global player_sprite_left_rect, player_sprite_right_rect, player_sprite_standing_rect, player_sprite_up_rect
+                global current_sprite
+                #global target_ball1_y, target_ball2_y, target_ball3_y
                 if attack_beginning:
                     attack_start_time = pygame.time.get_ticks()
                     attack_beginning = False
@@ -422,12 +430,40 @@ while running:
                 screen.blit(dodge_item_ball, ball1_rect)
                 screen.blit(dodge_item_ball, ball2_rect)
                 screen.blit(dodge_item_ball, ball3_rect)
+                target_ball1_x, target_ball1_y = 660 ,1080
+                target_ball2_x, target_ball2_y = 960 ,1080
+                target_ball3_x, target_ball3_y = 1110 ,1080
                 elapsed = pygame.time.get_ticks() - attack_start_time
                 if elapsed >= 2000:
-                    ball1_rect = move_dodge_item(ball1_rect, (width/2 - 300, 1080), dodge_speed)
-                    ball2_rect = move_dodge_item(ball2_rect, (width/2 , 1080), dodge_speed)
-                    ball3_rect = move_dodge_item(ball3_rect, (width/2 + 150, 1080), dodge_speed)
-            phase1_attack_3_normal_fast_balls1()
+                    ball1_rect = move_dodge_item(ball1_rect, (target_ball1_x, target_ball1_y), dodge_speed)
+                    ball2_rect = move_dodge_item(ball2_rect, (target_ball2_x, target_ball2_y), dodge_speed)
+                    ball3_rect = move_dodge_item(ball3_rect, (target_ball3_x, target_ball3_y), dodge_speed)
+                player_left_mask = pygame.mask.from_surface(player_sprite_left)
+                player_right_mask = pygame.mask.from_surface(player_sprite_right)
+                player_standing_mask = pygame.mask.from_surface(player_sprite_standing)
+                player_up_mask = pygame.mask.from_surface(player_sprite_up)
+                ball_mask1 = pygame.mask.from_surface(dodge_item_ball1)
+                ball_mask2 = pygame.mask.from_surface(dodge_item_ball2)
+                ball_mask3 = pygame.mask.from_surface(dodge_item_ball3)
+
+                player_mask = pygame.mask.from_surface(current_sprite)
+
+                for ball_rect, ball_mask in zip([ball1_rect, ball2_rect, ball3_rect], [ball_mask1, ball_mask2, ball_mask3]):
+                    offset = (ball_rect.x - current_sprite_rect.x, ball_rect.y - current_sprite_rect.y)
+                    if player_mask.overlap(ball_mask, offset):
+                        print("hit")
+                        return True
+                
+                if ball1_rect.y >= target_ball1_y and ball2_rect.y >= target_ball2_y and ball3_rect.y >= target_ball3_y:
+                    return False
+            #phase1_attack_3_normal_fast_balls1()
+            dead = phase1_attack_3_normal_fast_balls1()
+            if dead:
+                break
+            elif dead == False:
+                next_attack = True
+                print("next attack")
+            
     if keys[K_ESCAPE]:
         pygame.quit()
         sys.exit()
